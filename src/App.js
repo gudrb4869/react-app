d6bc21201e7cbedbd33315d33d6b1dc99129c8c0
HyungKyuPark <gudrb4869@naver.com>
2023-01-13 18:49:57 +0900

    컴포넌트 이벤트 및 CRUD 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,148 @@
 import React, { Component } from 'react';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 import Subject from './components/Subject';
+import Control from './components/Control';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.max_content_id = 3;
+    this.state = {
+      mode:'welcome',
+      selected_content_id:2,
+      subject:{title:'WEB', sub:'world wide web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+  getReadContent(){
+    var i = 0;
+    while (i < this.state.contents.length) {
+      var data = this.state.contents[i];
+      if (data.id == this.state.selected_content_id) {
+        return data;
+      }
+      i = i + 1;
+    }
+  }
+  getContent(){
+    var _title, _desc, _article = null;
+    if (this.state.mode == 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if (this.state.mode == 'read') {
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    } else if (this.state.mode == 'create') {
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        // add content to this.state.contents
+        this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id, title:_title, desc:_desc}
+        // );
+        // push 사용하지 않는 이유는 TOC.js의 shouldComponentUpdate 때문
+        // 원본 데이터와 바뀐 데이터 비교해서 렌더링 여부 결정하려고 concat 사용함
+        // var _contents = this.state.contents.concat(
+        //   {id:this.max_content_id, title:_title, desc:_desc}
+        // )
+        // push가 쓰고싶으면 Array.from 통해 복제해서 문제해결
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
+        // 배열의 경우 Array.from
+        // var a = [1,2]; var b = Array.from(a); a==b는 false.
+        // b.push(3); console.log(a, b, a==b)
+        // 객체의 경우 Object.assign
+        // var a = {name:'egoing'}; var b = Object.assign({}, a); a==b는 false.
+        // b.name = 'leezche' console.log(a, b, a==b)
+        // var newContents = Array.from(this.state.contents);
+        // newContents.push({id:this.max_content_id, title:_title, desc:_desc});
+        this.setState({
+          contents:_contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
+          // contents:newContents
+        });
+      }.bind(this)}></CreateContent>
+    } else if (this.state.mode == 'update') {
+      var _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc){
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while (i < _contents.length) {
+            if (_contents[i].id == _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
+          }
+        this.setState({
+          contents:_contents,
+          mode:'read'
+        });
+      }.bind(this)}></UpdateContent>
+    }
+    return _article;
+  }
   render() {
+    console.log('App render');
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({
+              mode:'welcome'
+            });
+          }.bind(this)}
+        >
+        </Subject>
+        <TOC
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
+        <Control onChangeMode={function(_mode){
+          if (_mode === 'delete') {
+            if(window.confirm('really?')){
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while (i < _contents.length) {
+                if (_contents[i].id === this.state.selected_content_id) {
+                  _contents.splice(i,1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              })
+              alert('delete!');
+            }
+          } else {
+            this.setState({
+              mode:_mode
+            });
+          }
+          
+        }.bind(this)}></Control>
+        {this.getContent()}
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
-    render() {
-      return (
-        <nav>
-            <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">JavaScript</a></li>
-            </ul>
-        </nav>
-      );
+  shouldComponentUpdate(newProps, newState) {
+    console.log('====>TOC render shouldComponentUpdate'
+      ,newProps.data
+      ,this.props.data
+    );
+    if(this.props.data == newProps.data){
+      return false;
     }
+    return true;
   }
+  render() {
+    console.log('====>TOC render');
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while(i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/"+data[i].id}
+            // 자바스크립트 속성을 이용하는 방식
+            data-id={data[i].id}
+            onClick={function(e){
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}
+            // 자바스크립트 속성을 이용하는 방식
+            // 자바스크립트 속성을 이용하지 않는 방식
+            // onClick={function(id, e){
+            //   e.preventDefault();
+            //   this.props.onChangePage(id);
+            // }.bind(this, data[i].id)}
+            // 자바스크립트 속성을 이용하지 않는 방식
+            >{data[i].title}</a>
+        </li>)
+      i = i + 1;
+    }
+    return (
+      <nav>
+          <ul>
+              {lists}
+          </ul>
+      </nav>
+    );
+  }
+}
 
-  export default TOC;
+export default TOC;
